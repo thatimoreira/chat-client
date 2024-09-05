f6f1c8b5c5754dcfd8ddacbe1f067a3d0be2d291
thatimoreira <thatimoreira.dev@gmail.com>
2024-09-05 14:31:03 -0300

    Adiciona camada responsável pelas regras de negócio e que chama a camada Repository que fará as requisições HTTP para servidor

---
+++ client/service.py
@@ -0,0 +1,14 @@
+# Regras de negócio, validações e chama Repository, que faz operações com os dados
+
+from client import repository
+
+def create_message(data):
+    sender = data.get('sender')
+    text = data.get('text')
+
+    message = {
+        "sender": sender,
+        "text": text
+    }
+
+    return repository.save_message(message)
